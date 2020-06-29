99b66411b7bad7805a149bd2258b70459bee8bbf
ashjare <55306533+ashjare@users.noreply.github.com>
2020-06-29 13:54:03 +0530

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,27 @@
+package com;
+import java.util.*;
+
+public class calculator {
+   
+    public int add(int first, int second) {
+
+    	return  first + second;
+            
+    }
+    public int sub(int first, int second) {
+
+    	return  first - second;
+            
+    }
+    public int mul(int first, int second) {
+
+    	return  first * second;
+            
+    }
+    public double div(double first, double second) {
+
+    	return  first / second;
+            
+    }
+   
+}
